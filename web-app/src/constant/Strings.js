126bdde1a3b54140f1a08487dec3cd2cd39bdf51
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2023-03-10 20:09:08 +0800

    add strings

---
+++ web-app/src/constant/Strings.js
@@ -0,0 +1,8 @@
+export const Strings = {
+  WELCOME_CHALLENGE: 'Welcome to the Trivia Challenge!',
+  WELCOME_CHALLENGE_DESC:
+    'You will be presented with 10 True or False questions.',
+  SCORE_QUESTION: 'Can you score 10/10?',
+  LETS_START: `LET'S START`,
+  PLAY_AGAIN: 'PLAY AGAINn',
+};
