1fbc4c8490ec03d73d6e706e93af04089be39b20
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2023-03-10 20:08:19 +0800

    modify app

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import HomePage from './pages/home'
+import { HomePage, QuizPage } from './pages/';
 
-import './styles/main.scss'
+import './styles/main.scss';
+import { StyledBackground } from './components';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route element={<HomePage />} path="/" />
-        </Routes>
+      <div className='App'>
+        <StyledBackground>
+          <Routes>
+            <Route element={<HomePage />} path='/' />
+            <Route element={<QuizPage />} path='quiz' />
+          </Routes>
+        </StyledBackground>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
