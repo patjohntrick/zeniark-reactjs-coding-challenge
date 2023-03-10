eaa67e81703131e620b2417b3b02100b1ef09d52
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2023-03-10 16:34:47 +0800

    choose data and create file

---
+++ web-app/src/constant/Data.js
@@ -0,0 +1,90 @@
+export const data = [
+  {
+    category: 'Entertainment: Music',
+    type: 'boolean',
+    difficulty: 'easy',
+    question: 'Scatman John&#039;s real name was John Paul Larkin.',
+    correct_answer: 'True',
+    incorrect_answers: ['False'],
+  },
+  {
+    category: 'Entertainment: Video Games',
+    type: 'boolean',
+    difficulty: 'easy',
+    question:
+      '&quot;Metal Gear Solid 3: Snake Eater&quot; was released in 2004.',
+    correct_answer: 'True',
+    incorrect_answers: ['False'],
+  },
+  {
+    category: 'Geography',
+    type: 'boolean',
+    difficulty: 'easy',
+    question: 'Hungary is the only country in the world beginning with H.',
+    correct_answer: 'False',
+    incorrect_answers: ['True'],
+  },
+  {
+    category: 'Entertainment: Japanese Anime & Manga',
+    type: 'boolean',
+    difficulty: 'easy',
+    question:
+      'In the &quot;To Love-Ru&quot; series, Golden Darkness is sent to kill Lala Deviluke.',
+    correct_answer: 'False',
+    incorrect_answers: ['True'],
+  },
+  {
+    category: 'History',
+    type: 'boolean',
+    difficulty: 'easy',
+    question:
+      'The French Kingdom helped the United States gain their independence over Great Britain during the Revolutionary War.',
+    correct_answer: 'True',
+    incorrect_answers: ['False'],
+  },
+  {
+    category: 'Science & Nature',
+    type: 'boolean',
+    difficulty: 'easy',
+    question:
+      'Water always boils at 100&deg;C, 212&deg;F, 373.15K, no matter where you are.',
+    correct_answer: 'False',
+    incorrect_answers: ['True'],
+  },
+  {
+    category: 'Entertainment: Music',
+    type: 'boolean',
+    difficulty: 'easy',
+    question:
+      'The 2011 movie &quot;The Adventures of Tintin&quot; was directed by Steven Spielberg.',
+    correct_answer: 'True',
+    incorrect_answers: ['False'],
+  },
+  {
+    category: 'Science: Mathematics',
+    type: 'boolean',
+    difficulty: 'easy',
+    question:
+      'An isosceles triangle has two sides of equal length as opposed to three.',
+    correct_answer: 'True',
+    incorrect_answers: ['False'],
+  },
+  {
+    category: 'Entertainment: Television',
+    type: 'boolean',
+    difficulty: 'easy',
+    question:
+      '&quot;The Simpsons&quot; family is named after creator Matt Groening&#039;s real family.',
+    correct_answer: 'True',
+    incorrect_answers: ['False'],
+  },
+  {
+    category: 'Entertainment: Books',
+    type: 'boolean',
+    difficulty: 'easy',
+    question:
+      'Shub-Niggurath is a creature that was created by \tJ. R. R. Tolkien in his novel &quot;The Lord of The Rings&quot;.',
+    correct_answer: 'False',
+    incorrect_answers: ['True'],
+  },
+];
