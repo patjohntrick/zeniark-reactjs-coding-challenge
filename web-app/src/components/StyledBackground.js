20d62cba756eab05cff70df26f2688f958e63d59
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2023-03-11 13:19:34 +0800

    modify background

--- web-app/src/components/StyledBackground.js
+++ web-app/src/components/StyledBackground.js
@@ -5,7 +5,7 @@ export const StyledBackground = ({ children }) => {
   return (
     <div className='container'>
       <img className='img-style' src={background} alt='Zeniark Logo' />
-      {children}
+      <div className='background-inner-container'>{children}</div>
     </div>
   );
 };
