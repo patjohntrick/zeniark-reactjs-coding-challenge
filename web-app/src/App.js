518beb6352f13d272bcb87041200077078ee9b14
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2023-03-11 11:46:29 +0800

    removed unecessary

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,23 +1,54 @@
-import React from 'react';
+import React, { useContext, createContext, useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { HomePage, QuizPage } from './pages/';
-
+import { HomePage, QuizPage, ResultPage } from './pages/';
 import './styles/main.scss';
 import { StyledBackground } from './components';
+import { useRandomQuestions } from './hooks/';
+import { data } from './constant/';
+
+export const AppContext = createContext();
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [finalTenQuestions, setFinalTenQuestions] = useState([]);
+  const [score, setScore] = useState(0);
+  const { randomData, isError } = useRandomQuestions(data);
+
+  // useEffect(() => {
+  //   if (!isError) {
+  //     setQuestions(randomData);
+  //   }
+  // }, []);
+
+  // console.log(questions);
+
   return (
-    <BrowserRouter>
-      <div className='App'>
-        <StyledBackground>
-          <Routes>
-            <Route element={<HomePage />} path='/' />
-            <Route element={<QuizPage />} path='quiz' />
-          </Routes>
-        </StyledBackground>
-      </div>
-    </BrowserRouter>
+    <AppContext.Provider
+      value={{
+        questions,
+        setQuestions,
+        score,
+        setScore,
+        userAnswers,
+        setUserAnswers,
+        finalTenQuestions,
+        setFinalTenQuestions,
+      }}
+    >
+      <BrowserRouter>
+        <div className='App'>
+          <StyledBackground>
+            <Routes>
+              <Route element={<HomePage />} path='/' />
+              <Route element={<QuizPage />} path='quiz/' />
+              <Route element={<ResultPage />} path='quiz/result/' />
+            </Routes>
+          </StyledBackground>
+        </div>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 }
 
